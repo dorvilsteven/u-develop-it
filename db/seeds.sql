2a040d713cc4beb783ad4ff98f4ed0fd0191a15b
StevenD <dorvilsteven@gmail.com>
2021-11-12 20:05:44 -0500

    create, insert, edit, and delete table data, created files in db

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+insert into candidates (first_name, last_name, industry_connected)
+values
+    ('Virginia', 'Woolf', 1),
+    ('Piers', 'Gaveston', 1),    
+    ('Charles', 'LeRoi', 0),  
+    ('Katherine', 'Mansfield', 1), 
+    ('Dora', 'Carrington', 1),      
+    ('Edward', 'Bellamy', 0),  
+    ('Unica', 'Zurb', 0); 
