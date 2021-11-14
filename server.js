ed71179604d80eeee8b2250cb0c28833e1d0ab77
StevenD <dorvilsteven@gmail.com>
2021-11-14 14:48:44 -0500

    database queries

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3005;
@@ -6,6 +7,31 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Neverneeded1!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {console.log(err)};
+    console.log(row);
+});
+
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {console.log(err)}
+    console.log(result);
+});
 
 // default response for not found request
 app.use((req, res) => {
