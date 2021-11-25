42e44d867ed56b32b1b139f1b7b17458aaac3654
StevenD <dorvilsteven@gmail.com>
2021-11-25 11:15:18 -0500

    add parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,12 @@
-create table candidates (
-    id integer auto_increment primary key,
-    first_name varchar(30) not null,
-    last_name varchar(30) not null,
-    industry_connected boolean not null
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE candidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    industry_connected BOOLEAN NOT NULL
 );
